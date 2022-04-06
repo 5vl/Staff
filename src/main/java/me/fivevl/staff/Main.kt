2eec2367bd1675798464c40287f3ba99362eec4e
5vl <nederlandmax@gmail.com>
2022-04-06 22:14:10 +0200

    Deleted Main.java and edited some features.

--- src/main/java/me/fivevl/staff/Main.kt
+++ src/main/java/me/fivevl/staff/Main.kt
@@ -17,13 +17,11 @@ class Main : JavaPlugin() {
             DONE:
 
             NOT DONE:
-                - Staff mode
-                - Freeze
-                - Inventory Lookup (Open inventory)
+                - Staff mode (Invisibility, fly, items)
+                - Freeze (Command and freeze wand in staff mode)
+                - Open inventory (Command and wand in staff mode) with permission for being able to edit it
                 - Teleport to online players
-                - Invisibility
                 - Knockback Stick (Extra, useful to check if the player has no kb)
-                - PlaceholderAPI
-                - Toggle on/off each individual items, (Extra, per-player settings would be sweet)
+                - PlaceholderAPI support
     */
 }
